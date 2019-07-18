1f75c3bb60a3feadabba9834ee07bb017d0100c6
h2ck4u <h2ckforu@gmail.com>
2019-07-18 23:46:28 +0900

    task3/Editor에 mouseup,down이벤트 등록 및 popup연동

--- src/js/manager/MouseEventManager.js
+++ src/js/manager/MouseEventManager.js
@@ -2,6 +2,38 @@ class MouseEventManager {
     constructor(editorId, editor) {
         this.editorId = editorId;
         this.editor = editor;
+        this.popup = editor.popup;
+        this.selectionManager = editor.selectionManager;
+
+        this.attachEvent();
+    }
+
+    /**
+     * 에디터에 필요한 이벤트들을 등록합니다.
+     */
+    attachEvent() {
+        this.mouseDownOnEditor();
+        this.mouseUpOnEditor();
+    }
+
+    /**
+     * Editor에 MouseDown 이벤트를 등록합니다.
+     */
+    mouseDownOnEditor() {
+        const elEditor = $(`#${this.editorId}`);
+        elEditor.on('mousedown', () => {
+            this.popup.hide();
+        });
+    }
+
+    /**
+     * Editor에 MouseUp 이벤트를 등록합니다.
+     */
+    mouseUpOnEditor() {
+        const elEditor = $(`#${this.editorId}`);
+        elEditor.on('mouseup', () => {
+            this.popup.show();
+        })
     }
 }
 
