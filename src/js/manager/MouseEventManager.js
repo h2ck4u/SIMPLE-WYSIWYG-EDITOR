bf5de3856d35434d86d7b65f474955ed14c8fae7
h2ck4u <h2ckforu@gmail.com>
2019-07-25 15:26:06 +0900

    review15/ EventManager Arrow Function으로 변경.

--- src/js/manager/MouseEventManager.js
+++ src/js/manager/MouseEventManager.js
@@ -11,62 +11,52 @@ class MouseEventManager {
     /**
      * 에디터에 필요한 이벤트들을 등록합니다.
      */
-    attachEvent() {
+    attachEvent = () => {
         const $editor = this.editor.getElement();
-        this.mouseDownOnEditor($editor);
-        this.mouseUpOnEditor($editor);
-        this.mouseDownOnButton();
-        this.mouseUpOnButton();
+        $editor.on('mousedown', this.onEditorMouseDown);
+        $editor.on('mouseup', this.onEditorMouseUp);
+
+        this.popup.buttons.forEach(button => {
+            let $button = button.getElement();
+            $button.on('mousedown', button, this.onButtonMouseDown);
+            $button.on('mouseup', this.onButtonMouseUp);
+        });
     }
 
     /**
      * Editor에 MouseDown 이벤트를 등록합니다.
      */
-    mouseDownOnEditor($editor) {
-        $editor.on('mousedown', () => {
-            this.uiController.togglePopup();
-        });
+    onEditorMouseDown = () => {
+        this.uiController.togglePopup();
     }
 
     /**
      * Editor에 MouseUp 이벤트를 등록합니다.
      */
-    mouseUpOnEditor($editor) {
-        $editor.on('mouseup', () => {
-            setTimeout(() => {
-                this.uiController.togglePopup();
-            }, 0);
-        });
+    onEditorMouseUp = () => {
+        setTimeout(() => {
+            this.uiController.togglePopup();
+        }, 0);
     }
 
     /**
      * Button에 MouseDown 이벤트를 등록합니다.
      */
-    mouseDownOnButton() {
-        this.popup.buttons.forEach(button => {
-            let $button = button.getElement();
-            $button.on('mousedown', (e) => {
-                const buttonName = e.target.getAttribute('name');
-                if (!!buttonName) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    this.textController.execCommand(buttonName);
-                    this.uiController.setButtonActive(button);
-                }
-            });
-        });
+    onButtonMouseDown = (e) => {
+        const buttonName = e.target.getAttribute('name');
+        if (!!buttonName) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.textController.execCommand(buttonName);
+            this.uiController.setButtonActive(e.data);
+        }
     }
 
     /**
      * Button에 MouseUp 이벤트를 등록합니다.
      */
-    mouseUpOnButton() {
-        this.popup.buttons.forEach(button => {
-            let $button = button.getElement();
-            $button.on('mouseup', (e) => {
-                e.stopPropagation();
-            });
-        });
+    onButtonMouseUp = (e) => {
+        e.stopPropagation();
     }
 }
 
